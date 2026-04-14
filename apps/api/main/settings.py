dedc05cfd31768826a62378c0b1e51db155055b6
Besay GonzalezBenavides Garcia <besaygg@gmail.com>
2026-04-14 18:23:20 +0100

    Habilitadas las peticiones a la api

--- apps/api/main/settings.py
+++ apps/api/main/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'checklists.apps.ChecklistsConfig',
     'shared.apps.SharedConfig',
     'django_rq',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
 ROOT_URLCONF = 'main.urls'
 
